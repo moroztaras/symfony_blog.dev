26ae3fd24fd3007cfbc04d60c4c71d8a83223c5b
moroztaras <moroztaras@i.ua>
2017-08-31 15:40:29 +0300

    generate get & set metods

--- src/BlogBundle/Entity/Blog.php
+++ src/BlogBundle/Entity/Blog.php
@@ -2,16 +2,152 @@
 
 namespace BlogBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Blog
+ * @package BlogBundle\Entity
+ * @ORM\Table(name="blog")
+ * @ORM\Entity
+ */
 
 class Blog
 {
+   /**
+    * @ORM\Id
+    * @ORM\Column(type="integer")
+    * @ORM\GeneratedValue(strategy="AUTO")
+    */
     private $id;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $title;
 
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
     private $summary;
 
+    /**
+     * @ORM\Column(type="text")
+     */
     private $body;
 
+    /**
+     * @ORM\Column(type="datetime")
+     */
     private $created;
+
+    public function __construct()
+    {
+        $this->created = new \DateTime();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Blog
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set summary
+     *
+     * @param string $summary
+     *
+     * @return Blog
+     */
+    public function setSummary($summary)
+    {
+        $this->summary = $summary;
+
+        return $this;
+    }
+
+    /**
+     * Get summary
+     *
+     * @return string
+     */
+    public function getSummary()
+    {
+        return $this->summary;
+    }
+
+    /**
+     * Set body
+     *
+     * @param string $body
+     *
+     * @return Blog
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * Get body
+     *
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return Blog
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
 }
