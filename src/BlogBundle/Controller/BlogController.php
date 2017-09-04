8bf791c018f3fbfbaff5b4ad66a04b836699d00d
moroztaras <moroztaras@i.ua>
2017-09-04 14:16:22 +0300

    T9 create main menu

--- src/BlogBundle/Controller/BlogController.php
+++ src/BlogBundle/Controller/BlogController.php
@@ -8,7 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class BlogController extends Controller
 {
     /**
-     * @Route("/blogs", name="blogs")
+     * @Route("/", name="homepage")
      */
     public function indexAction()
     {
