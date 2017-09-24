519cea4ca4b0a243e76f4564a175139eecf85ba2
moroztaras <moroztaras@i.ua>
2017-09-24 15:02:55 +0300

    T11 design blog pages

--- config.rb
+++ config.rb
@@ -3,9 +3,9 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "web/bundles/blog/css"
+css_dir = "web/css"
 sass_dir = "sass"
-images_dir = "web/bundles/blog/images"
+images_dir = "web/images"
 javascripts_dir = "web/bundles/blog/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
