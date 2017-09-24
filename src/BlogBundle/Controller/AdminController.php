02167c210b059f2d660845cc344820ab004fd9f5
moroztaras <moroztaras@i.ua>
2017-09-24 21:15:55 +0300

    T13 setting admin security

---
+++ src/BlogBundle/Controller/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function indexAction()
+    {
+        return new Response("HERE");
+    }
+
+}
