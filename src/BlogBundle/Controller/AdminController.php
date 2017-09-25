85d7414108e3587d10f17a7082586307315b56ff
moroztaras <moroztaras@i.ua>
2017-09-25 15:38:17 +0300

    T14 created admin panel

--- src/BlogBundle/Controller/AdminController.php
+++ src/BlogBundle/Controller/AdminController.php
@@ -16,4 +16,33 @@ class AdminController extends Controller
         return new Response("HERE");
     }
 
+    /**
+     * @Route("/admin/blogs", name="admin_blogs")
+     */
+    public function blogsAction()
+    {
+        $blogs = $this->getDoctrine()->getRepository("BlogBundle:Blog")->findAll();
+
+        return $this->render(
+            "BlogBundle:Admin:admin_view.html.twig", [
+                "blogs"=>$blogs
+                ]
+        );
+    }
+
+    /**
+     * @Route("/admin/blog/{id}/edit", name="admin_blog_edit")
+     */
+    public function editAction($id)
+    {
+        return new Response($id);
+    }
+
+    /**
+     * @Route("/admin/blog/{id}/delete", name="admin_blog_delete")
+     */
+    public function deleteAction($id)
+    {
+        return new Response($id);
+    }
 }
